04202fe8b5d5f6a69c82e234e4e480dc9c2a99b6
Stephen Moore <judo2000@comcast.net>
2022-10-29 05:26:24 -0600

    Create fallback text in case there are no expenses

--- App.js
+++ App.js
@@ -62,7 +62,7 @@ export default function App() {
   };
   return (
     <>
-      <StatusBar style='auto' />
+      <StatusBar style='light' />
       <ExpensesContextProvider>
         <NavigationContainer>
           <Stack.Navigator
