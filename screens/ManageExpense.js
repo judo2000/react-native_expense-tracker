79a3d28486a8c1596967aad89b673519b09b315f
Stephen Moore <judo2000@comcast.net>
2022-10-28 21:18:10 -0600

    Create a custom button to create the cancel and add button on the ManageExpense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 
@@ -19,8 +20,19 @@ const ManageExpense = ({ route, navigation }) => {
     console.log('deleting me punk?');
   };
 
+  const cancelHandler = () => {};
+  const confirmHandler = () => {};
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button style={styles.button} mode='flat' onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -42,6 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
